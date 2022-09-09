ee56238fe3eec784be59e59411258ce3699fcf75
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-09 23:49:27 +0100

    Update result

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,50 +7,6 @@ import '../App.css';
 // Create an array for the data
 const calculator = ['0', 'AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
-// class Calculator extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       total: '0',
-//       next: '',
-//       operation: '',
-//     };
-//     this.handleCalculation = this.handleCalculation.bind(this);
-//   }
-
-//   handleCalculation(e) {
-//     const btnName = e.target.value;
-//     this.setState((data) => calculate(data, btnName));
-//   }
-
-//   outPut() {
-//     const { total, next, operation } = this.state;
-//     const result = `${total || ''} ${operation || ''} ${next || ''}`;
-//     return result;
-//   }
-
-//   render() {
-//     return (
-//       <section className="section">
-//         <div className="container">
-//           {
-//             calculator.map((item, index) => {
-//               if (index === 0) {
-//                 return (
-// <input type="button"
-// key={index} readOnly className={`item${index} + items`} value={this.outPut()} />
-//                 );
-//               }
-// return <input type="button"
-// onClick={this.handleCalculation} className={`item${index} + items`} key={index} value={item} />;
-//             })
-//           }
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-
 const Calculator = () => {
   const [state, setState] = useState({
     total: '0',
@@ -65,7 +21,7 @@ const Calculator = () => {
 
   const outPut = () => {
     const { total, next, operation } = state;
-    const result = `${total || ''} ${next || ''} ${operation || ''}`;
+    const result = `${total || ''} ${operation || ''} ${next || ''}`;
     return (result);
   };
 

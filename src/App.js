0aa5fd57aad444f54fcebd6d35f0608e92031c5b
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-09 15:47:18 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCalculation = this.handleCalculation.bind('this');
+    this.state = {
+      total: '5',
+      next: '',
+      operation: '',
+    };
+  }
+
+  handleCalculation(newTotal) {
+    const { total, next, operation } = this.state;
+    console.log(`Total is ${total}, next is ${next} & operation is ${operation}`);
+    this.setState({ total: newTotal });
+  }
+
   render() {
+    const { total } = this.state;
     return (
       <div className="App">
-        <Calculator />
+        <Calculator
+          result={total}
+          handleCalculation={this.handleCalculation}
+        />
       </div>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,24 +1,53 @@
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import calculate from '../logic/calculate';
-import operate from '../logic/operate';
+import PropTypes from 'prop-types';
+// import calculate from '../logic/calculate';
+// import operate from '../logic/operate';
 import '../App.css';
 
 // Create an array for the data
-const calculator = ['0', 'AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-const design = calculator.map((item, index) => (
-  <p className={`item${index} + items`} key={index}>{item}</p>
-));
+const calculator = ['0', 'AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props.result);
+    this.handleCalculation = this.handleCalculation.bind('this');
+  }
+
+  handleCalculation(e) {
+    const { result } = this.props;
+    const { handleCalculation } = this.props;
+    handleCalculation(e.target.value);
+    console.log((e.target.value), result);
+  }
+
   render() {
     return (
       <section className="section">
-        <div className="container" border={1}>{design}</div>
+        <div className="container">
+          {
+            calculator.map((item, index) => {
+              const { result } = this.props;
+              if (index === 0) {
+                return (
+                  <input type="button" key={index} readOnly className={`item${index} + items`} value={result} />
+                );
+              }
+              return <input type="button" onClick={this.handleCalculation} className={`item${index} + items`} key={index} value={item} />;
+            })
+          }
+        </div>
       </section>
     );
   }
 }
 
+Calculator.propTypes = {
+  result: PropTypes.string.isRequired,
+  handleCalculation: PropTypes.func.isRequired,
+};
 export default Calculator;
